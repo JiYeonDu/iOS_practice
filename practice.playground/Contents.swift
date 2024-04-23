aeb4839f6f7abba7033ab995f923a945f5ee6db1
신지연 <shinjy@sinjiyeon-ui-MacBookAir.local>
2024-04-23 16:18:51 +0900

    add : wishList

--- practice.playground/Contents.swift
+++ practice.playground/Contents.swift
@@ -778,7 +778,7 @@ func solution(_ sizes:[[Int]]) -> Int {
 }
 
 solution([[60, 50], [30, 70], [60, 30], [80, 40]])
- */
+
 
 func solution(_ s:String, _ n:Int) -> String {
     var charToAscii: [UInt8] = []
@@ -809,3 +809,30 @@ func solution(_ s:String, _ n:Int) -> String {
 }
 
 solution("a B z", 4)
+ */
+
+import Foundation
+
+
+func solution(_ s:String) -> Int {
+    let dic = ["one": 1, "two": 2, "three": 3, "four": 4,
+                              "five": 5, "six": 6, "seven": 7, "eight": 8,
+                              "nine": 9, "zero": 0]
+    var result: [Int] = []
+    var flag: String = ""
+    for char in s {
+        if let temp = Int(String(char)) {
+            result.append(temp)
+        } else {
+            flag.append(char)
+            if dic.keys.contains(flag) {
+                result.append(dic[flag]!)
+                flag = ""
+            }
+        }
+    }
+    let res = result.map { String($0) }.joined()
+    return Int(res)!
+}
+
+print(solution("2three45sixseven"))
